f51ccc8c9f549250fa4212f2dceafea180ef40f7
Samuel Soares <samuelsoares.academic@gmail.com>
2025-12-04 16:28:41 -0300

    Adicionando regra de excluir promoção quando o produto for deletado

--- src/Services/promotion-service.js
+++ src/Services/promotion-service.js
@@ -1,79 +1,45 @@
-import promotionModel from '../models/promotion-model.js';
 import productModel from '../models/product-model.js';
+import promotionModel from '../models/promotion-model.js'; 
 
-const promotionService = {
-    generateCode: async function() {
-        let code;
-        let exists = true;
-
-        // Tenta gerar até achar um que não existe
-        while (exists) {
-            code = Math.floor(1000 + Math.random() * 9000).toString();
-            const promo = await promotionModel.findOne({ code });
-            if (!promo) exists = false;
-        }
-        return code;
-    },
-
+const productService = {
     getAll: async function () {
-        // Busca todas as promoções
-        const promotions = await promotionModel.find({});
-        
-        const result = await Promise.all(promotions.map(async (promo) => {
-            const product = await productModel.findOne({ code: promo.targetProductCode });
-            
-            let promoObj = promo.toObject();
-
-            if (product) {
-                promoObj.productName = product.name;
-                promoObj.originalPrice = product.price;
-                
-                // Calcula o valor com desconto
-                const discountValue = product.price * (promo.discountPercentage / 100);
-                promoObj.discountedPrice = product.price - discountValue;
-            } else {
-                promoObj.productName = "Produto não encontrado";
-                promoObj.originalPrice = 0;
-                promoObj.discountedPrice = 0;
-            }
-
-            return promoObj;
-        }));
-
-        return result;
+        return await productModel.find({});
     },
 
     getOne: async function (code) {
-        return await promotionModel.findOne({ code: code });
+        const product = await productModel.findOne({ code: code });
+        if (!product) {
+            throw new Error("Produto não encontrado");
+        }
+        return product;
     },
 
     create: async function (data) {
-        const productExists = await productModel.findOne({ code: data.targetProductCode });
-        if (!productExists) throw new Error("O produto informado não existe");
-
-        // Gera o código automático de 4 dígitos
-        const newCode = await this.generateCode();
-
-        const newPromo = {
-            code: newCode,
-            discountPercentage: data.discountPercentage,
-            targetProductCode: data.targetProductCode
-        };
-
-        return await promotionModel.create(newPromo);
+        const productExists = await productModel.findOne({ code: data.code });
+        if (productExists) {
+            throw new Error("Já existe um produto com este código");
+        }
+        return await productModel.create(data);
     },
 
     update: async function (code, data) {
-        if (data.targetProductCode) {
-            const productExists = await productModel.findOne({ code: data.targetProductCode });
-            if (!productExists) throw new Error("O produto informado não existe");
+        const product = await productModel.findOne({ code: code });
+        if (!product) {
+            throw new Error("Produto não encontrado");
         }
-        return await promotionModel.findOneAndUpdate({ code: code }, data, { new: true });
+        return await productModel.findOneAndUpdate({ code: code }, data, { new: true });
     },
 
     delete: async function (code) {
-        return await promotionModel.deleteOne({ code: code });
+        const product = await productModel.findOne({ code: code });
+        if (!product) {
+            throw new Error("Produto não encontrado");
+        }
+
+        await promotionModel.deleteMany({ targetProductCode: code });
+
+        return await productModel.deleteOne({ code: code });
     }
 }
 
-export default promotionService;
+export default productService;
