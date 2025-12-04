9672294de77be7ce6caeee0ef82f28024ac9c24a
Samuel Soares <samuelsoares.academic@gmail.com>
2025-12-04 16:40:20 -0300

    Vonatndo lógica de delete para original

--- src/Services/promotion-service.js
+++ src/Services/promotion-service.js
@@ -1,45 +1,79 @@
+import promotionModel from '../models/promotion-model.js';
 import productModel from '../models/product-model.js';
-import promotionModel from '../models/promotion-model.js'; 
 
-const productService = {
+const promotionService = {
+    generateCode: async function() {
+        let code;
+        let exists = true;
+
+        // Tenta gerar até achar um que não existe
+        while (exists) {
+            code = Math.floor(1000 + Math.random() * 9000).toString();
+            const promo = await promotionModel.findOne({ code });
+            if (!promo) exists = false;
+        }
+        return code;
+    },
+
     getAll: async function () {
-        return await productModel.find({});
+        // Busca todas as promoções
+        const promotions = await promotionModel.find({});
+        
+        const result = await Promise.all(promotions.map(async (promo) => {
+            const product = await productModel.findOne({ code: promo.targetProductCode });
+            
+            let promoObj = promo.toObject();
+
+            if (product) {
+                promoObj.productName = product.name;
+                promoObj.originalPrice = product.price;
+                
+                // Calcula o valor com desconto
+                const discountValue = product.price * (promo.discountPercentage / 100);
+                promoObj.discountedPrice = product.price - discountValue;
+            } else {
+                promoObj.productName = "Produto não encontrado";
+                promoObj.originalPrice = 0;
+                promoObj.discountedPrice = 0;
+            }
+
+            return promoObj;
+        }));
+
+        return result;
     },
 
     getOne: async function (code) {
-        const product = await productModel.findOne({ code: code });
-        if (!product) {
-            throw new Error("Produto não encontrado");
-        }
-        return product;
+        return await promotionModel.findOne({ code: code });
     },
 
     create: async function (data) {
-        const productExists = await productModel.findOne({ code: data.code });
-        if (productExists) {
-            throw new Error("Já existe um produto com este código");
-        }
-        return await productModel.create(data);
+        const productExists = await productModel.findOne({ code: data.targetProductCode });
+        if (!productExists) throw new Error("O produto informado não existe");
+
+        // Gera o código automático de 4 dígitos
+        const newCode = await this.generateCode();
+
+        const newPromo = {
+            code: newCode,
+            discountPercentage: data.discountPercentage,
+            targetProductCode: data.targetProductCode
+        };
+
+        return await promotionModel.create(newPromo);
     },
 
     update: async function (code, data) {
-        const product = await productModel.findOne({ code: code });
-        if (!product) {
-            throw new Error("Produto não encontrado");
+        if (data.targetProductCode) {
+            const productExists = await productModel.findOne({ code: data.targetProductCode });
+            if (!productExists) throw new Error("O produto informado não existe");
         }
-        return await productModel.findOneAndUpdate({ code: code }, data, { new: true });
+        return await promotionModel.findOneAndUpdate({ code: code }, data, { new: true });
     },
 
     delete: async function (code) {
-        const product = await productModel.findOne({ code: code });
-        if (!product) {
-            throw new Error("Produto não encontrado");
-        }
-
-        await promotionModel.deleteMany({ targetProductCode: code });
-
-        return await productModel.deleteOne({ code: code });
+        return await promotionModel.deleteOne({ code: code });
     }
 }
 
-export default productService;
+export default promotionService;
